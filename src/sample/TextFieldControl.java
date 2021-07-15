060821a6f08795f0ad2139111e892bb799a6f801
glenchiridza <glenchiridz@gmail.com>
2021-07-15 15:11:00 +0200

    TextField Control

--- src/sample/TextFieldControl.java
+++ src/sample/TextFieldControl.java
@@ -1,7 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -12,6 +17,8 @@ import javafx.stage.Stage;
 public class TextFieldControl extends Application {
 
     //create message label
+    Label messageLabel = new Label("ENter you Name in to Field");
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -22,8 +29,34 @@ public class TextFieldControl extends Application {
         TextField firstNameFld = new TextField();
         TextField lastNameFld = new TextField();
 
+        firstNameFld.setPrefColumnCount(15);
+        lastNameFld.setPrefColumnCount(15);
+
+        firstNameFld.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                printMessage("You Have changed your First Name");
+            }
+        });
+
+        lastNameFld.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                printMessage("You have change your surname");
+            }
+        });
+
+        GridPane root = new GridPane();
+
+        root.setHgap(10);
+        root.setVgap(5);
+
+        root.addRow(0, messageLabel);
+        root.addRow(1, new Label("First Name"), firstNameFld);
+        root.addRow(2, new Label("Last Name"), lastNameFld);
 
         root.setMinSize(350, 250);
+
 
         root.setStyle(
                 "-fx-padding: 10;" +
@@ -35,7 +68,20 @@ public class TextFieldControl extends Application {
         );
 
 
+        Scene scene = new Scene(root);
 
+        stage.setScene(scene);
+
+        stage.setTitle("Text Area Practice");
+
+        stage.show();
+
+
+    }
+
+    private void printMessage(String msg) {
+
+        messageLabel.setText(msg);
     }
 
 
