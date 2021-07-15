d1228954828e797ad838e4fad0ef50bb969c794b
glenchiridza <glenchiridz@gmail.com>
2021-07-15 14:55:19 +0200

    Label Control

--- src/sample/Controls.java
+++ src/sample/Controls.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
@@ -29,7 +30,26 @@ public class Controls extends Application {
         lastNameLbl.setLabelFor(lastNameFld);
         lastNameLbl.setMnemonicParsing(true);
 
+        GridPane root = new GridPane();
+        root.addRow(0, firstNameLbl, firstNameFld);
+        root.addRow(1, lastNameLbl, lastNameFld);
 
+        root.setMinSize(350, 250);
+
+        root.setStyle(
+                "-fx-padding: 10;" +
+                        "-fx-border-style:solid inside;"+
+                        "-fx-border-width:2;"+
+                        "-fx-border-insets: 5;"+
+                        "-fx-border-radius:5;"+
+                        "-fx-border-color:blue;"
+        );
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Label Example");
+        stage.show();
 
 
 
