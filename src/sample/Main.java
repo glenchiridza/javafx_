ae8459bd8ff6daf64738596c7d3c6cbfc2f7d28a
glenchiridza <glenchiridz@gmail.com>
2021-07-15 14:38:13 +0200

    start app

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -15,7 +16,28 @@ public class Main extends Application {
         //create Text
         Text text = new Text("Hello JAVAFX");
 
-        //create
+        //create the VBOX
+        VBox root = new VBox();
+        //add the text to the VBOx
+        root.getChildren().add(text);
+        //set the size of VBOX
+        root.setMinSize(350,750);
+
+        //create the scene
+        Scene scene = new Scene(root);
+
+//        set the stage properties
+        primaryStage.setX(100);
+        primaryStage.setY(200);
+        primaryStage.setMinHeight(300);
+        primaryStage.setMinWidth(400);
+
+        //add the scene to the stage
+        primaryStage.setScene(scene);
+
+        primaryStage.setTitle("The Initial start FX app");
+
+        primaryStage.show();
     }
 
 
