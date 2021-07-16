917f3ecf18bed7103efc05037a3eba320bf4e89c
glenchiridza <glenchiridz@gmail.com>
2021-07-16 12:40:36 +0200

    RadioButton

--- src/sample/ToggleButtonControl.java
+++ src/sample/ToggleButtonControl.java
@@ -21,6 +21,7 @@ public class ToggleButtonControl extends Application {
 
     public static void main(String[] args) {
 
+        launch(args);
     }
     @Override
     public void start(Stage stage) throws Exception {
