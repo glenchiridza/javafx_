b87df12d9b344a38e1927f6945e8c8edf3b8c762
glenchiridza <glenchiridz@gmail.com>
2021-07-27 14:43:29 +0200

    create menu and menu item

--- src/sample/MenuExampleControl.java
+++ src/sample/MenuExampleControl.java
@@ -1,7 +1,67 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
 /**
  * Created by glenc on Jul 2021
  **/
-public class MenuExampleControl {
+public class MenuExampleControl extends Application {
+
+    Label messageLbl = new Label("Press any menu item to see message");
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        //create the menus
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+
+        //create the MenuItem New
+        MenuItem newItem = new MenuItem("New");
+        newItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                printMessage("You pressed the new Menu Item")
+            }
+        });
+
+        MenuItem openItem = new MenuItem("Open");
+        openItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                printMessage("You pressed the open Menu Item");
+            }
+        });
+
+
+
+
+        root.setStyle(
+                "-fx-padding: 10;" +
+                        "-fx-border-style:solid inside;"+
+                        "-fx-border-width:2;"+
+                        "-fx-border-insets: 5;"+
+                        "-fx-border-radius:5;"+
+                        "-fx-border-color:blue;"
+        );
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("ListView");
+        stage.show();
+
+    }
+
+    private void printMessage(String s) {
+        messageLbl.setText(s);
+    }
 }
