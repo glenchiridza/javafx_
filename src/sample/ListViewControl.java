66abe53ab21df85e16b92ab4ba4168b2e0feb50c
glenchiridza <glenchiridz@gmail.com>
2021-07-27 14:26:47 +0200

    list view done

--- src/sample/ListViewControl.java
+++ src/sample/ListViewControl.java
@@ -1,9 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,6 +23,23 @@ public class ListViewControl extends Application {
     public void start(Stage stage) throws Exception {
 
         ListView<String> cars = new ListView<>();
+        cars.getItems().addAll("Ford","Ferrari","Porsche");
+
+        cars.getSelectionModel().selectFirst();
+
+        cars.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String t1, String t2) {
+                selectionLabel.setText("You selected: "+t2);
+            }
+        });
+
+        GridPane root =  new GridPane();
+        root.setVgap(10);
+        root.setHgap(10);
+        root.addRow(0,cars);
+        root.addRow(1,selectionLabel);
+        root.setMinSize(350,350);
 
         root.setStyle(
                 "-fx-padding: 10;" +
